2bc5bed33dd575ead496d70d39b3f4d760ddf808
Hendridev <hendridevlp@gmail.com>
2020-12-23 09:57:09 +0700

    complete basic logic

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const card = document.querySelectorAll('.card');
 
 let arr = [];
 let right = [];
+let point = 0;
 
 function countplus() {
     count.textContent++;
@@ -14,12 +15,33 @@ function reset() {
     }
 }
 
+function resetAll() {
+    for (let i = 0; i < card.length; i++) {
+        card[i].classList.remove('complete');
+        point = 0;
+        count.textContent = 0;
+    }
+}
+
+
 function correct(el) {
     arr.sort((a, b) => {
         if (a == b) {
-
+            for (let i = 0; i < right.length; i++) {
+                right[i].classList.add('complete');
+                point += .5;
+            }
         }
     })
+}
+
+function congrats() {
+    if (point == 4) {
+        setTimeout(() => {
+            alert(`You win, click count: ${count.textContent}`);
+            resetAll();
+        }, 200);
+    }
 }
 
 let togcount = 0;
@@ -28,14 +50,18 @@ card.forEach((me) => {
         me.classList.toggle('hide');
         countplus();
         arr.push(me.textContent);
+        right.push(me);
+        correct(me);
+        congrats();
         togcount++;
         while (togcount == 2) {
             setTimeout(() => {
                 reset();
                 arr = [];
+                right = [];
             }, 200);
             togcount = 0;
         }
-        console.log(me);
+        console.log(point);
     })
 });
